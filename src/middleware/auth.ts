c46b524665fe1e679565f4d71cc0905cb6441bc3
Kavinda Dissanayake <kavinda.dissanayake@aventagelabs.com>
2024-08-29 22:20:28 +0530

    user profile update

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,7 +1,48 @@
+import { NextFunction, Request, Response } from "express";
 import { auth } from "express-oauth2-jwt-bearer";
+import jwt from "jsonwebtoken";
+import User from "../models/user";
+
+declare global {
+  namespace Express {
+    interface Request {
+      auth0Id: string;
+      userId: string;
+    }
+  }
+}
 
 export const jwtCheck = auth({
   audience: process.env.AUTH0_AUDIENCE as string,
   issuerBaseURL: process.env.AUTH0_ISSUER_BASE_URL as string,
   tokenSigningAlg: "RS256",
 });
+
+export const jwtParse = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const { authorization } = req.headers;
+
+  if (!authorization || !authorization.startsWith("Bearer ")) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+
+  const token = authorization.split("Bearer ")[1].trim();
+  try {
+    const decodded = jwt.decode(token) as jwt.JwtPayload;
+    const auth0Id = decodded?.sub;
+
+    const user = await User.findOne({ auth0Id });
+    if (!user) {
+      return res.status(401).json({ message: "Unauthorized" });
+    }
+
+    req.auth0Id = auth0Id as string;
+    req.userId = user._id.toString();
+    next();
+  } catch (err) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+};
